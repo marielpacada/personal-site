cf01b63445a0376eb70b63de9cd792543fb5c637
Mariel Pacada <49132861+marielpacada@users.noreply.github.com>
2021-01-27 14:39:25 -0800

    landing done

--- script.js
+++ script.js
@@ -1,35 +1,32 @@
-var isIntroTyped = false;
-
 function typeIntro() {
     var intro = document.getElementById("intro");
-    var typewriter = new Typewriter(intro, { delay: 100 });
+    var typewriter = new Typewriter(intro, {
+        delay: 100,
+        onCreateTextNode: typeDesc()
+    });
 
     typewriter.start();
     typewriter.typeString("hey, my name is ...");
-    isIntroTyped = true;
 
-    setTimeout(() => { this.typeDesc(isIntroTyped); }, 3500);
-    typewriter.pauseFor(7000);
+    // setTimeout(() => { this.typeDesc(); }, 2500);
+    typewriter.pauseFor(1800);
     typewriter.deleteChars(3);
     typewriter.typeString(" ariel?");
-    typewriter.pauseFor(5000);
+    typewriter.pauseFor(1800);
     typewriter.deleteChars(6);
-    typewriter.typeString("mariel!");
-
-
+    typewriter.typeString("mariel •ᴗ•");
 }
 
-function typeDesc(bool) {
-    if (bool) {
-        var name = document.getElementById("name");
-        var typewriter = new Typewriter(name, {cursor:'', delay:20});
+function typeDesc() {
+    var name = document.getElementById("name");
+    var typewriter = new Typewriter(name, { cursor: '', delay: 20 });
 
-        typewriter.start();
-        typewriter.typeString("like the little mermaid");
-        typewriter.pauseFor(3000);
-        typewriter.typeString(" with an m in front.");
-
-    }
-
+    
+    typewriter.start();
+    typewriter.pauseFor(2500);
+    typewriter.typeString("like the little mermaid");
+    typewriter.pauseFor(2500);
+    typewriter.typeString(' with an "m" in front.');
 }
+
 
