b4fe1e78a1ef3f5576b8f290a88e81340c0702a7
Mariel Pacada <49132861+marielpacada@users.noreply.github.com>
2021-03-23 14:28:51 -0700

    fix common js

--- nav.js
+++ nav.js
@@ -1,11 +1,13 @@
 $(function () {
-    if ($(window).width() < 1025) {
-        $("#nav").css("justify-content", "center");
-    }
+
+
+
 
     if ($(window).height() > $("body").height()) {
         $("footer").addClass("foot-short");
     } else {
         $("footer").removeClass("foot-short");
     }
+
+
 });
