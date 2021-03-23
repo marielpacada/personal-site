c25bcf53112948b7a260b25a2d8e546144feb383
Mariel Pacada <49132861+marielpacada@users.noreply.github.com>
2021-03-23 13:34:48 -0700

    fix footer

--- nav.js
+++ nav.js
@@ -1,7 +1,11 @@
-if ($(window).width() < 1025) {
-    $("#nav").css("justify-content", "center");
-}
+$(function () {
+    if ($(window).width() < 1025) {
+        $("#nav").css("justify-content", "center");
+    }
 
-if ($(window).height() > $("body").height()) {
-    $("footer").addClass("foot-short");
-}
+    if ($(window).height() > $("body").height()) {
+        $("footer").addClass("foot-short");
+    } else {
+        $("footer").removeClass("foot-short");
+    }
+});
