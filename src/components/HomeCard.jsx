7e1f75f041c8901e9bc766c56eaa075be52f481c
Mariel Pacada <49132861+marielpacada@users.noreply.github.com>
2021-06-06 14:51:55 -0700

    work on home card

--- src/components/HomeCard.jsx
+++ src/components/HomeCard.jsx
@@ -4,21 +4,38 @@ import Typewriter from 'typewriter-effect';
 function HomeCard(props) {
     return (
         <div className="home">
+            <div className="typewriter">
+                <div className="type-head">
+                    <Typewriter
+                        options={{ delay: 100 }}
+                        onInit={(typewriter) => {
+                            typewriter.start()
+                                .typeString("hey, my name is ...")
+                                .pauseFor(1500)
+                                .deleteChars(3)
+                                .typeString(" ariel?")
+                                .pauseFor(1800)
+                                .deleteChars(6)
+                                .typeString("mariel!");
+                        }}
+                    />
+                </div>
+                <div className="type-subhead">
+                    <Typewriter
+                        options={{ cursor: '', delay: 20 }}
+                        onInit={(typewriter) => {
+                            typewriter.start()
+                                .pauseFor(2500)
+                                .typeString("like the little mermaid")
+                                .pauseFor(2500)
+                                .typeString(' with an "m" before.')
+                        }}
+                    />
 
-            <Typewriter
-                onInit={(typewriter) => {
-                    typewriter.typeString('Hello World!')
-                        .callFunction(() => {
-                            console.log('String typed out!');
-                        })
-                        .pauseFor(2500)
-                        .deleteAll()
-                        .callFunction(() => {
-                            console.log('All strings were deleted');
-                        })
-                        .start();
-                }}
-            />
+                </div>
+
+            </div>
+
 
         </div>
     );
