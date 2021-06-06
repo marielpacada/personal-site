3d0f48c40aca2a63d8dc38016c19c94f3b308746
Mariel Pacada <49132861+marielpacada@users.noreply.github.com>
2021-06-06 11:33:41 -0700

    work on hover buttons

---
+++ src/components/Portrait.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Portrait() {
+    return <div className="portrait my-row-center">
+        <img src="images/pengwing.png" alt="portrait" className="nav-pic" />
+
+    </div>;
+}
+
+export default Portrait;
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import NavButton from "./NavButton";
+import Portrait from "./Portrait";
 
 function Sidebar() {
-    return <div className="my-col-start sidebar">
-    <NavButton iconName="user-circle" tabName="about"/>
-    <NavButton iconName="laptop-code" tabName="projects"/>
-    <NavButton iconName="comment-dots" tabName="musings"/>
-    <NavButton iconName="id-card" tabName="contact"/>
-    </div>;
+    return (
+        <div className="my-col-start sidebar">
+            <Portrait />
 
+            <NavButton iconName="user-circle" tabName="about" />
+            <NavButton iconName="laptop-code" tabName="projects" />
+            <NavButton iconName="comment-dots" tabName="musings" />
+            <NavButton iconName="id-card" tabName="contact" />
+        </div>
+    );
 }
 
 export default Sidebar;
