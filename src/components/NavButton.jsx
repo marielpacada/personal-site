c6d2e088b3fc963b9e504a8b2da00c1ef1f1d2d8
Mariel Pacada <49132861+marielpacada@users.noreply.github.com>
2021-06-07 09:15:50 -0700

    fix button lessssgoooo

--- src/components/NavButton.jsx
+++ src/components/NavButton.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 function NavButton(props) {
     return (
         <div className="my-row-start nav-button">
+            <div className="nav-button-overlay"></div>
             <div className="tab-icon"><i className={"fas fa-".concat(props.iconName)} /></div>
             <div className="my-row-center"><p>{props.tabName}</p></div>
         </div>
